c999877f604ad02429ccfc71a757d28885dc7ecc
Murilo Polese <murilopolese@gmail.com>
2018-05-24 17:50:15 +0100

    `hexToBase64Colors` converts a single frame only

--- rpk.js
+++ rpk.js
@@ -85,29 +85,27 @@ class PixelKit extends BaseRPCDevice {
     }
 
     hexToBase64Colors(element) {
-        // return colorsArray.map(element => {
-            let frameBuffer = new Buffer(element.length * 2, 0);
+        let frameBuffer = new Buffer(element.length * 2, 0);
 
-            element.forEach((color, index) => {
-                let colorBin = new Buffer(2),
-                    rgb888,
-                    rgb565;
+        element.forEach((color, index) => {
+            let colorBin = new Buffer(2),
+                rgb888,
+                rgb565;
 
-                if (typeof color === "string" && color.length === 7 && /#[0-9a-f]{6}/i.test(color)) {
-                    rgb888 = parseInt(color.substring(1, 7), 16);
-                    //                blue                 green                  red
-                    rgb565 = (rgb888 & 0xF8) >> 3 | (rgb888 & 0xFC00) >> 5 | (rgb888 & 0xF80000) >> 8;
+            if (typeof color === "string" && color.length === 7 && /#[0-9a-f]{6}/i.test(color)) {
+                rgb888 = parseInt(color.substring(1, 7), 16);
+                //                blue                 green                  red
+                rgb565 = (rgb888 & 0xF8) >> 3 | (rgb888 & 0xFC00) >> 5 | (rgb888 & 0xF80000) >> 8;
 
-                    colorBin.writeUInt16BE(rgb565, 0);
-                } else {
-                    // If the color is invalid, write black
-                    colorBin.writeUInt16BE(0x0000, 0);
-                }
+                colorBin.writeUInt16BE(rgb565, 0);
+            } else {
+                // If the color is invalid, write black
+                colorBin.writeUInt16BE(0x0000, 0);
+            }
 
-                colorBin.copy(frameBuffer, index * 2);
-            });
-            return frameBuffer.toString('base64');
-        // });
+            colorBin.copy(frameBuffer, index * 2);
+        });
+        return frameBuffer.toString('base64');
     }
 
     /**
@@ -120,6 +118,7 @@ class PixelKit extends BaseRPCDevice {
         return this.rpcRequest('device-info', []);
     }
 
+    // frame must be an array with 128 hexadecimal colors prefixed with a `#`
     streamFrame(frame) {
         let encodedFrame = this.hexToBase64Colors(frame);
         return this.rpcRequest('lightboard:on', [{ map: encodedFrame }]);
@@ -145,19 +144,58 @@ class PixelKit extends BaseRPCDevice {
     connectToWifi(ssid, password) {
         return this.rpcRequest('wifi-connect', [ssid, password]);
     }
-    
-    playTone(freq, duration) {}
-    isPlayingTone() {}
-    stopTone() {}
-    getMicThreshold() {}
-    setMicThreshold(level) {}
+
+    playTone(freq, duration) {
+        return this.rpcRequest('play-tone', [{ freq: freq, duration: duration }]);
+    }
+    isPlayingTone() {
+        return this.rpcRequest('is-playing-tone', []);
+    }
+    stopTone() {
+        return this.rpcRequest('stop-tone', []);
+    }
+    getMicThreshold() {
+        return this.rpcRequest('get-mic-threshold', []);
+    }
+    // `level` can be `high`, `low` and `mid` or `custom`. If it's `custom`
+    // it's expected to set a `min` and a `max`.
+    setMicThreshold(level, min, max) {
+        if(level == 'custom') {
+            return this.rpcRequest('set-mic-threshold', [{
+                level: level, min: min, max: max
+            }]);
+        }
+        return this.rpcRequest('set-mic-threshold', [{ level: level }]);
+    }
 
     // disableOTGControl() {}
-    getAnimationConfig(modeId) {}
-    setAnimationConfit(name, modeId, frameCount, frameRate, coverUrl) {}
-    getMaxFrames() {}
-    eraseAnimation(modeId) {}
-    saveAnimationFrame(modeId, frameNumber, frame) {}
+    getAnimationConfig(modeId) {
+        return this.rpcRequest('get-anim-config', [{ modeId: modeId }]);
+    }
+    setAnimationConfig(name, modeId, frameCount, frameRate, coverUrl) {
+        return this.rpcRequest('set-anim-config', [{
+            name: name,
+            modeId: modeId,
+            frameCount: frameCount,
+            frameRate: frameRate,
+            animImgUrl: coverUrl
+        }]);
+    }
+    getMaxFrames() {
+        return this.rpcRequest('get-max-frames', []);
+    }
+    eraseAnimation(modeId) {
+        return this.rpcRequest('animation-erase', [{ modeId: modeId }]);
+    }
+    saveAnimationFrame(modeId, frameNumber, frame) {
+        let encodedFrame = this.hexToBase64Colors(frame);
+        return this.rpcRequest('animation-send-frame', [{
+            modeId: modeId,
+            frameNumber: frameNumber,
+            map: encodedFrame
+        }]);
+    }
+    // TODO: Save animation that writes the frames and save the config.
 }
 
 module.exports = PixelKit;
